cd51bc3a7774cb911c885b7f1b7c849792eaa696
Nikita Budiakov <budyakov91@gmail.com>
2025-05-19 18:32:51 -0400

    added security packages, error handler, and CRUD for job api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,25 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors).map(val => val.message).join(',');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate field value entered for ${Object.keys(err.keyValue)} field, please chose another value.`
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `Invalid ID: ${err.value}, please provide a valid ID`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
